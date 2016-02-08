6032ebf8cb9a8bbd27fa2531a731c43cd169efce
akaygorodova <akaygorodova@issart.com>
2016-02-08 12:12:48 +0700

    Step 2

--- complex/src/main/java/javalearn/Complex.java
+++ complex/src/main/java/javalearn/Complex.java
@@ -4,4 +4,20 @@ package javalearn;
  * Created by anutakay@gmail.com on 08.02.2016.
  */
 public class Complex {
+
+    public void setReal(double real) {
+
+    }
+
+    public double getReal() {
+        return 0;
+    }
+
+    public  void setImage(double real) {
+
+    }
+
+    public double getImage() {
+        return 0;
+    }
 }
