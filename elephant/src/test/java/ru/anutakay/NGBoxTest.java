8dc0a3d315107b9b16c6fa6f4c858c0a746fb3e4
anutakay <anutakay@gmail.com>
2016-02-14 20:50:23 +0700

    test class added

---
+++ elephant/src/test/java/ru/anutakay/NGBoxTest.java
@@ -0,0 +1,16 @@
+package ru.anutakay;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by anutakay@gmail.com on 14.02.2016.
+ */
+public class NGBoxTest {
+
+    @Test
+    public void test() {
+        assertEquals(0, 0);
+    }
+}
