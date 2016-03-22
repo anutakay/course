079d73b6f7c1941e08f6414b17daf45caf26fc76
A.Kaygorodova <akaygorodova@issart.com>
2016-03-22 21:21:04 +0700

    Checkroom draft

--- elephant/src/main/java/ru/anutakay/Checkroom.java
+++ elephant/src/main/java/ru/anutakay/Checkroom.java
@@ -1,11 +1,15 @@
 package ru.anutakay;
 
+import ru.anutakay.exception.EmptyException;
+import ru.anutakay.exception.FullException;
+import ru.anutakay.exception.SizeException;
+
 /**
  * Created by anutakay@gmail.com on 22.03.2016.
  */
 public interface Checkroom {
-    String put(Freezable freezable);
-    Freezable get(String key);
+    String put(Freezable freezable) throws FullException, SizeException;
+    Freezable get(String key) throws EmptyException;
     boolean hasFreeSpace();
     int maxCapacity();
     int usedCapacity();
--- elephant/src/main/java/ru/anutakay/CheckroomImpl.java
+++ elephant/src/main/java/ru/anutakay/CheckroomImpl.java
@@ -1,36 +1,81 @@
 package ru.anutakay;
 
+import ru.anutakay.exception.EmptyException;
+import ru.anutakay.exception.FullException;
+import ru.anutakay.exception.SizeException;
+import ru.anutakay.exception.UncompatibleValueException;
+
+import java.util.*;
+
 /**
  * Created by akaygorodova@issart.com on 22.03.2016.
  */
 public class CheckroomImpl extends PlaceImpl implements Checkroom {
 
+    private int capacity;
+
+    Set<String> emptyCells;
+
+    Map<String, Freezable> objects;
+
     public CheckroomImpl(Size size, int capacity) {
         super(size);
+        this.capacity = capacity;
+        emptyCells = new HashSet<String>();
     }
 
     @Override
-    public String put(Freezable freezable) {
-        return null;
+    public String put(Freezable freezable) throws FullException, SizeException {
+        if(!isFits(freezable)) {
+            throw new SizeException();
+        }
+        if(!this.hasFreeSpace()) {
+            throw new FullException();
+        }
+        String cell = getFirstEmptyCell();
+        objects.put(cell, freezable);
+        emptyCells.remove(cell);
+        return cell;
     }
 
     @Override
-    public Freezable get(String key) {
-        return null;
+    public Freezable get(String key) throws EmptyException {
+        boolean empty = emptyCells.contains(key);
+        if(empty) {
+            throw new EmptyException();
+        }
+        boolean filled = objects.keySet().contains(key);
+        if(!filled) {
+            throw new UncompatibleValueException();
+        }
+        Freezable obj = objects.get(key);
+        emptyCells.add(key);
+        return obj;
     }
 
     @Override
     public boolean hasFreeSpace() {
-        return false;
+        return maxCapacity() > usedCapacity();
     }
 
     @Override
     public int maxCapacity() {
-        return 0;
+        return capacity;
     }
 
     @Override
     public int usedCapacity() {
-        return 0;
+        return objects.size();
+    }
+
+    private String getFirstEmptyCell() {
+        String res;
+        if(emptyCells.isEmpty()) {
+            res = UUID.randomUUID().toString();
+        } else {
+            res = emptyCells.iterator().next();
+            emptyCells.remove(res);
+        }
+        return res;
     }
 }
