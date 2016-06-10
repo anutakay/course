8bce5defb63a592559c7d9210c6f57e7403bdc63
A.Kaygorodova <akaygorodova@issart.com>
2016-06-10 21:05:39 +0700

    fixes

--- regex/src/test/java/ru/anutakay/EmailFinderTest.java
+++ regex/src/test/java/ru/anutakay/EmailFinderTest.java
@@ -26,8 +26,8 @@ public class EmailFinderTest
                 "This time,tester+100@gmail.com for some reason, admin_123@issart-test.ABC.CoM I decided babama@gmail.com.1a to take a look at my underlying nowhere.@where.com assumptions. I had never actually read (or even skimmed) the RFC for an email address. I simply based google123@gmail.a my implementation on my 1xxxx@abc.com preconceived assumptions about what makes a valid email address user-100500@yahoo-test.com. You know what they say about !#$&'*+-/=?^_`{}|~@example.com assuming.\n" +
                 "What I crokodile@yahoo.com found out .askold@port-artur.com was surprising. Nearly 100% of regular expressions on the web purporting to validate imyapolzovatelya@aol.com an email address are too strict.\n" +
                 "It turns ivan-100@issart.com out that the local part of an email address tra-ta-ta.100@google.com, the part before the @ sign, allows pipets.100@kiev.com.ua a lot more characters than you’d expect. According to section zayats12345@@.com.com of RFC 2821 which defines SMTP gmail@gmail.com@gmail.com, the part before the @ sign is called the local part (the part after being the host domain john.smith(comment)@example.com) and it is only intended to be interpreted by the receiving host short@1.com.";
-        EmailFinder finder = new EmailFinder(source);
-        String[] result = finder.findEmails();
+        EmailFinder finder = new EmailFinder();
+        String[] result = finder.findEmailByCandidate(source);
         for (int i = 0; i < result.length; i++) {
             assertTrue(result[i].equals(validEmails[i]));
         }
@@ -36,16 +36,16 @@ public class EmailFinderTest
     @org.testng.annotations.Test
     public void empty() {
         String source = "Before.";
-        EmailFinder finder = new EmailFinder(source);
-        String[] result = finder.findEmails();
+        EmailFinder finder = new EmailFinder();
+        String[] result = finder.findEmailByCandidate(source);
         assertEquals(result.length, 0);
     }
 
     @org.testng.annotations.Test
     public void simple() {
         String source = "akaygorodova@issart.com";
-        EmailFinder finder = new EmailFinder(source);
-        String[] result = finder.findEmails();
+        EmailFinder finder = new EmailFinder();
+        String[] result = finder.findEmailByCandidate(source);
         assertEquals(result.length, 1);
         assertEquals(result[0], source);
     }
@@ -53,8 +53,8 @@ public class EmailFinderTest
     @org.testng.annotations.Test
     public void simple_extract() {
         String source = "aaaaaaaaaa akaygorodova@issart.com aaaaaaaaaaa";
-        EmailFinder finder = new EmailFinder(source);
-        String[] result = finder.findEmails();
+        EmailFinder finder = new EmailFinder();
+        String[] result = finder.findEmailByCandidate(source);
         assertEquals(result.length, 1);
         assertEquals(result[0], "akaygorodova@issart.com");
     }
@@ -62,8 +62,8 @@ public class EmailFinderTest
     @org.testng.annotations.Test
     public void dot_extract() {
         String source = "aaaaaaaaaa akaygorodova@issart.com. aaaaaaaaaaa";
-        EmailFinder finder = new EmailFinder(source);
-        String[] result = finder.findEmails();
+        EmailFinder finder = new EmailFinder();
+        String[] result = finder.findEmailByCandidate(source);
         assertEquals(result.length, 1);
         assertEquals(result[0], "akaygorodova@issart.com");
     }
@@ -71,8 +71,8 @@ public class EmailFinderTest
     @org.testng.annotations.Test
     public void dot_extract2() {
         String source = "aaaaaaaaaa .akaygorodova@issart.com. aaaaaaaaaaa";
-        EmailFinder finder = new EmailFinder(source);
-        String[] result = finder.findEmails2();
+        EmailFinder finder = new EmailFinder();
+        String[] result = finder.findEmails2(source);
         assertEquals(result.length, 1);
         assertEquals(result[0], "akaygorodova@issart.com");
     }
@@ -80,8 +80,8 @@ public class EmailFinderTest
     @org.testng.annotations.Test
     public void bracket_extract() {
         String source = "aaaaaaaaaa akaygorodova@issart.com) aaaaaaaaaaa";
-        EmailFinder finder = new EmailFinder(source);
-        String[] result = finder.findEmails();
+        EmailFinder finder = new EmailFinder();
+        String[] result = finder.findEmailByCandidate(source);
         assertEquals(result.length, 1);
         assertEquals(result[0], "akaygorodova@issart.com");
     }
@@ -89,8 +89,8 @@ public class EmailFinderTest
     @org.testng.annotations.Test
     public void bracket_extract2() {
         String source = "aaaaaaaaaa .akaygorodova@issart.com) aaaaaaaaaaa";
-        EmailFinder finder = new EmailFinder(source);
-        String[] result = finder.findEmails2();
+        EmailFinder finder = new EmailFinder();
+        String[] result = finder.findEmails2(source);
         assertEquals(result.length, 1);
         assertEquals(result[0], "akaygorodova@issart.com");
     }
