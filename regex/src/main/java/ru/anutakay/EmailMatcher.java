93e9a46321cc8ae3eb47f0518f1515d0b5077677
A.Kaygorodova <akaygorodova@issart.com>
2016-06-10 19:07:56 +0700

    regex fixes

--- regex/src/main/java/ru/anutakay/EmailMatcher.java
+++ regex/src/main/java/ru/anutakay/EmailMatcher.java
@@ -1,29 +1,14 @@
 package ru.anutakay;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Created by akaygorodova@issart.com on 12.05.2016.
  */
-public class EmailMatcher {
+public class EmailMatcher  {
 
-    final static String patternString = "(([0-9a-zA-Z][a-zA-Z0-9\\-'_\\+]*\\.?)+[a-zA-Z0-9][a-zA-Z0-9\\-'_\\+]*@([a-zA-Z0-9][a-zA-Z0-9\\-]*\\.)+[a-zA-Z]{2,})";
-
-    String source;
-
-    Matcher matcher;
+    EmailMatcherDelegate matcher;
 
     EmailMatcher(String source) {
-        this.source = source;
-        Pattern pattern = Pattern.compile(patternString);
-        matcher = pattern.matcher(source);
-    }
-
-    public boolean isEmail() {
-        Pattern pattern = Pattern.compile("^" + patternString + "$");
-        matcher = pattern.matcher(source);
-        return matcher.matches();
+        matcher = new EmailMatcherDelegate(source);
     }
 
     public boolean find() {
@@ -31,12 +16,7 @@ public class EmailMatcher {
     }
 
     public String email() {
-        String email;
-        try {
-            email = matcher.group(1);
-        } catch (IllegalStateException e) {
-            throw new IllegalStateException("No email found");
-        }
-        return email;
+        return matcher.email();
     }
+
 }
